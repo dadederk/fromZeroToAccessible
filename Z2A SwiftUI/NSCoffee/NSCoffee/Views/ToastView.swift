f2202318a87ae52750e28456f10f64bb5042e121
Rob Whitaker <rw@rwapp.co.uk>
2025-03-05 11:32:48 +0000

    Updated toast presentation/animation logic to allow for queued toasts

--- Z2A SwiftUI/NSCoffee/NSCoffee/Views/ToastView.swift
+++ Z2A SwiftUI/NSCoffee/NSCoffee/Views/ToastView.swift
@@ -8,36 +8,78 @@
 import SwiftUI
 
 struct ToastView: View {
+    enum AnimationDirection {
+        case animateIn, animateOut
+    }
     @Binding var message: String?
     @State var opacity = 0.0
+    @State var toastDelay: Task<Void, Never>?
+    @State var visibleMessage: String?
+
+    private var removeAfterDelay: Task<Void, Never>? {
+        Task {
+            do {
+                try await Task.sleep(for: .seconds(3.0))
+
+                withAnimation(.linear(duration: 0.2)) {
+                    opacity = 0.0
+
+                } completion: {
+                    visibleMessage = nil
+                }
+            } catch {}
+        }
+    }
+
+    func animateToast(_ direction: AnimationDirection, completion: (() -> Void)? = nil) {
+        withAnimation(.linear(duration: 0.2)) {
+            switch direction {
+            case .animateIn:
+                opacity = 1.0
+
+            case .animateOut:
+                opacity = 0.0
+            }
+        } completion: {
+            completion?()
+        }
+    }
 
     var body: some View {
         VStack {
             Spacer()
 
-            if let message = message {
-                Group {
-                    Text(message)
-                        .padding()
-                }
-                .background(.gray)
-                .cornerRadius(20)
-                .opacity(0.90)
-                .opacity(opacity)
-                .padding(.bottom)
-                .onAppear {
-                    withAnimation(.linear(duration: 0.2)) {
-                        opacity = 1.0
+            Group {
+                Text(visibleMessage ?? "")
+                    .padding()
+            }
+            .background(.gray)
+            .cornerRadius(20)
+            .opacity(0.90)
+            .opacity(opacity)
+            .padding(.bottom)
+            .onChange(of: message) { oldValue, newValue in
+                guard newValue != nil else { return }
 
-                    } completion: {
-                        withAnimation(.linear(duration: 0.2)
-                            .delay(3.0)) {
-                                opacity = 0.0
+                if visibleMessage != nil {
+                    toastDelay?.cancel()
 
-                            } completion: {
-                                self.message = nil
-                            }
+                    animateToast(.animateOut) {
+                        visibleMessage = newValue
+                        animateToast(.animateIn)
+                        toastDelay = removeAfterDelay
                     }
+
+                    message = nil
+
+                } else {
+
+                    toastDelay?.cancel()
+                    visibleMessage = newValue
+                    animateToast(.animateIn)
+
+                    toastDelay = removeAfterDelay
+                    message = nil
                 }
             }
         }
