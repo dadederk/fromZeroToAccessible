fd535c2aee6372da04a2595514241bedcad6bea1
Rob Whitaker <rw@rwapp.co.uk>
2025-03-07 17:56:23 +0000

    Moving add button on detail to bottom of screen

--- Z2A SwiftUI/NSCoffee/NSCoffee/Views/DrinkDetail.swift
+++ Z2A SwiftUI/NSCoffee/NSCoffee/Views/DrinkDetail.swift
@@ -15,29 +15,66 @@ struct DrinkDetail: View {
     @Binding var toastMessage: String?
 
     var body: some View {
-        List {
-            ZStack {
-                DrinkTableImage(imageName: drink.imageName)
+        VStack(spacing: 0) {
+            List {
+                ZStack {
+                    DrinkTableImage(imageName: drink.imageName)
 
-                VStack {
-                    Spacer()
+                    VStack {
+                        Spacer()
 
-                    Text(drink.description)
-                        .padding()
+                        Text(drink.description)
+                            .padding()
+                    }
                 }
-            }
 
-            Section("Extra Shots") {
-                ExtraShotsView(shotPrice: drink.shotPrice, extras: $extras)
-            }
+                Section("Extra Shots") {
+                    ExtraShotsView(shotPrice: drink.shotPrice, extras: $extras)
+                }
 
-            Section("Rate your drink") {
-                RatingView()
-            }
+                Section("Rate your drink") {
+                    RatingView()
+                }
 
-            MilkTypeView()
+                MilkTypeView()
+            }
+            .listStyle(.grouped)
+
+            Button {
+                if let order = order {
+                    basket.add(order)
+
+                } else {
+                    basket.add(Order(drink: drink))
+                }
+
+                toastMessage = "\(drink.name) added to cart"
+
+            } label: {
+                HStack {
+                    Image(systemName: "cart.fill.badge.plus")
+                        .padding(.horizontal, 4)
+
+                    Text("Add")
+                        .padding(.leading, 4)
+
+                    if let order = order {
+                        Text(CurrencyFormatter.format(order.perDrinkPrice))
+
+                    } else {
+                        Text(CurrencyFormatter.format(drink.basePrice))
+                    }
+
+                }
+                .padding(.vertical, 4)
+                .frame(maxWidth: .infinity)
+
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.horizontal)
+
         }
-        .listStyle(.grouped)
+        .background(Color(UIColor.groupTableViewBackground))
         .onChange(of: extras) { oldValue, newValue in
             if newValue.count > 0 {
                 order = nil
@@ -47,36 +84,5 @@ struct DrinkDetail: View {
             }
         }
         .navigationTitle(drink.name)
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    if let order = order {
-                        basket.add(order)
-
-                    } else {
-                        basket.add(Order(drink: drink))
-                    }
-
-                    toastMessage = "\(drink.name) added to cart"
-
-                } label: {
-                    HStack {
-                        Image(systemName: "cart.fill.badge.plus")
-
-                        VStack {
-                            Text("Add")
-
-                            if let order = order {
-                                Text(CurrencyFormatter.format(order.perDrinkPrice))
-
-                            } else {
-                                Text(CurrencyFormatter.format(drink.basePrice))
-                            }
-                        }
-                    }
-                }
-                .buttonStyle(.borderedProminent)
-            }
-        }
     }
 }
