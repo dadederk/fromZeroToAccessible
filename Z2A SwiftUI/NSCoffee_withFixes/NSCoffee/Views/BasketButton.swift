4e7f400c517e004ca495b7d2ea4417fec35e8d1f
Rob Whitaker <rw@rwapp.co.uk>
2025-03-09 12:36:22 +0200

    cart button fixes

--- Z2A SwiftUI/NSCoffee_withFixes/NSCoffee/Views/BasketButton.swift
+++ Z2A SwiftUI/NSCoffee_withFixes/NSCoffee/Views/BasketButton.swift
@@ -18,8 +18,11 @@ struct BasketButton: View {
                 }
             } label: {
                 ZStack(alignment: .topTrailing) {
-                    Image(systemName: "basket.fill")
-                    
+                    /* Fix: Using the same icon here as the
+                     detail screen is important for consistency
+                     */
+                    Image(systemName: "cart.fill")
+
                     if basket.orderCount > 0 {
                         HStack(spacing: 0) {
                             Text("\(basket.orderCount)")
@@ -32,6 +35,25 @@ struct BasketButton: View {
                         .clipShape(Capsule())
                     }
                 }
+            }
+        /* Fix: The button here had no accessible label,
+         this means that when the cart is empty iOS uses
+         the system-provided label for the SFSymbol.
+         When an item is in the cart the label is the number
+         of items. This change in label is very confusing.
+         Instead we'll set a fixed label describing the button,
+         and provide the number of items as a value.
+         */
+            .accessibilityLabel("Cart")
+            .accessibilityValue("\(basket.orderCount) item")
+
+        /* Fix: By default the Large Content Viewer
+         will show the button's icon. It is important
+         to show the number of items in the cart too.
+         */
+            .accessibilityShowsLargeContentViewer {
+                Image(systemName: "cart.fill")
+                Text("Cart, \(basket.orderCount) item")
             }
     }
 }
