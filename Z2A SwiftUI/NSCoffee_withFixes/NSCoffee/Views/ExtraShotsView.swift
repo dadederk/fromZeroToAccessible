be219a955cc66299d169ed4968029d8ce2e0ecea
Rob Whitaker <rw@rwapp.co.uk>
2025-03-09 13:58:23 +0200

    stacking shots price at larger sized

--- Z2A SwiftUI/NSCoffee_withFixes/NSCoffee/Views/ExtraShotsView.swift
+++ Z2A SwiftUI/NSCoffee_withFixes/NSCoffee/Views/ExtraShotsView.swift
@@ -11,39 +11,56 @@ struct ExtraShotsView: View {
     let shotPrice: Double
     @State private var shots = 0
     @Binding var extras: [Extra]
+    @Environment(\.dynamicTypeSize.isAccessibilitySize) var accessibilitySize
 
     private var extraShotPrice: Double {
         shotPrice * Double(shots)
     }
 
+    private var extraShotPriceText: Text {
+        Text("+ \(CurrencyFormatter.format(extraShotPrice))")
+    }
+
     var body: some View {
-        HStack {
-            Image(systemName: "minus.circle")
-                .onTapGesture {
-                    guard shots > 0 else { return }
+        VStack {
+            HStack {
+                Image(systemName: "minus.circle")
+                    .onTapGesture {
+                        guard shots > 0 else { return }
 
-                    shots -= 1
+                        shots -= 1
 
-                    if shots == 0 {
-                        extras = []
-                    } else {
+                        if shots == 0 {
+                            extras = []
+                        } else {
+                            extras = [Extra(description: "Extra shot", price: shotPrice, quantity: shots)]
+                        }
+                    }
+                    .foregroundStyle(.tint)
+
+                Text("numberShots.\(shots)")
+
+                Image(systemName: "plus.circle")
+                    .onTapGesture {
+                        guard shots < 4 else { return }
+
+                        shots += 1
                         extras = [Extra(description: "Extra shot", price: shotPrice, quantity: shots)]
                     }
+                    .foregroundStyle(.tint)
+
+                /* Fix: At larger text sizes moving content
+                 stacked horizontally to being stacked
+                 vertically allows for improved readability
+                 */
+                if !accessibilitySize {
+                    extraShotPriceText
                 }
-                .foregroundStyle(.tint)
+            }
 
-            Text("numberShots.\(shots)")
-
-            Image(systemName: "plus.circle")
-                .onTapGesture {
-                    guard shots < 4 else { return }
-
-                    shots += 1
-                    extras = [Extra(description: "Extra shot", price: shotPrice, quantity: shots)]
-                }
-                .foregroundStyle(.tint)
-
-            Text("+ \(CurrencyFormatter.format(extraShotPrice))")
+            if accessibilitySize {
+                extraShotPriceText
+            }
         }
     }
 }
